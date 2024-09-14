e1bc4bf9953ade7148cc414cc9baa83489123777
Carlos Alvarado Martinez <carlos-alvarado@outlook.com>
2024-09-14 11:10:21 -0400

    assignment 7 complete

--- misc-modules/hello.c
+++ misc-modules/hello.c
@@ -7,13 +7,13 @@ MODULE_LICENSE("Dual BSD/GPL");
 
 static int hello_init(void)
 {
-	printk(KERN_ALERT "Hello, world\n");
+	printk(KERN_ALERT "Hello, calvarado2004\n");
 	return 0;
 }
 
 static void hello_exit(void)
 {
-	printk(KERN_ALERT "Goodbye, cruel world\n");
+	printk(KERN_ALERT "Goodbye, calvarado2004\n");
 }
 
 module_init(hello_init);
